ee529720112778442b58e917efac32b7c9c3eba4
Daniel Loomer <fuzetsu490@gmail.com>
2013-10-26 02:55:32 -0400

    mal-comparator: improved folder structure

---
+++ mal_comparator/js/background.js
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------
+//                      JUST PLAYING AROUND
+// ----------------------------------------------------------
+
+chrome.extension.onMessage.addListener(function(req, sender, sendRes) {
+	console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+	if(req.purpose == "open_tab") {
+		chrome.windows.create({ url: req.url, incognito: true });
+	}
+});
